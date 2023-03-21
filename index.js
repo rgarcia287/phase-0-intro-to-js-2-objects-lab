f1febac587994722bc9a1841ec0e0202ef984eac
Roberto <robertogarcia287@gmail.com>
2023-03-21 17:23:50 -0500

    Roberto's Object Lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+var employee = {
+    name: "Dylan"
+}
+
+
+function updateEmployeeWithKeyAndValue(employee, name, value) {
+    const newEmployee = {...employee};
+    newEmployee[name] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, name, value) {
+    const newEmployee = {employee};
+    newEmployee[name] = value;
+    delete newEmployee.name;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee) {
+    delete employee.name;
+    return employee;
+}
+
